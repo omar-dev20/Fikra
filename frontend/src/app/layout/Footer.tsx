cbf74b54984059a280214aee54497bd7cb4bdb39
omar-dev20 <oboss5527@gmail.com>
2026-09-19 03:39:48 +0300

    edit tailwindcss to look good in mobile

--- frontend/src/app/layout/Footer.tsx
+++ frontend/src/app/layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="mt-8 border-t border-black/10 dark:border-white/10 ">
       <div className="mx-auto  flex w-full max-w-6xl items-center justify-between px-4 py-6 font-semibold text-base text-muted-foreground md:px-6">
-        <p className="mx-auto">
+        <p className="mx-auto text-center  ">
           <FormattedMessage
             id="footer.copyright"
             values={{ year: new Date().getFullYear() }}
